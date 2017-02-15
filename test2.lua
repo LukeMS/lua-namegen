aafe5dd944a427a76b68b1090bdcab768108d1a8
Lucas de Morais Siqueira <lucas.morais.siqueira@gmail.com>
2017-02-14 23:23:24 -0200

    Delete test2.lua

--- test2.lua
+++
@@ -1,9 +0,0 @@
-local inspect = require("inspect")
-local namegen = require("namegen")
-
-local rules = namegen.exhaust_rules("books")
-print(inspect(rules))
-local names = namegen.exhaust_set("books")
-for _, v in pairs(names) do
-    print(v)
-end
